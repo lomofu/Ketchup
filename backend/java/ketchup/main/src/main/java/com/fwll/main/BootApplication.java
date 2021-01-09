ed7b6610756f459a5eee32d203e497553faf5cb0
lomofu <lomofu@foxmail.com>
2021-01-09 13:13:53 +0000

    [demo1] init database and rest api

--- backend/java/ketchup/main/src/main/java/com/fwll/main/BootApplication.java
+++ backend/java/ketchup/main/src/main/java/com/fwll/main/BootApplication.java
@@ -4,7 +4,9 @@ import com.fwll.common.utils.SpringContextHelper;
 import com.fwll.main.config.AppConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author lomofu
@@ -12,6 +14,8 @@ import org.springframework.context.annotation.Import;
  * @create 08/Jan/2021 00:12
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.fwll.*.dao")
+@ComponentScan(basePackages = "com.fwll.*")
 @Import(value = SpringContextHelper.class)
 public class BootApplication {
   public static void main(String[] args) {
